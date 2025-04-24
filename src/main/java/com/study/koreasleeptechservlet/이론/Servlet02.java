5c7d782e5907c6a7ba0c477d0e30a069ff68b7a7
96paradise <cnw0806@naver.com>
2025-04-24 10:47:55 +0900

    20250424 CNW

---
+++ src/main/java/com/study/koreasleeptechservlet/이론/Servlet02.java
@@ -0,0 +1,15 @@
+package com.study.koreasleeptechservlet.이론;
+
+public class Servlet02 {
+    /*
+     * === Sevlet 프로젝트 생성 ===
+     *
+     * cf) IntelliJ Ultimate 필요
+     *       : IntelliJ Community Edition은 서블릿/JSP 웹 프로젝트를 지원하지 x
+     *
+     * 1. 필수 설치 프로그램
+     *   - IntelliJ Ultimate Edition
+     *   - JDK(JDK 17)
+     *   - Apache Tomcat (9.x OR 10
+     * */
+}
