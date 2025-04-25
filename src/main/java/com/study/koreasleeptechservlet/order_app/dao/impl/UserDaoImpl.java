db2490367eb6da7f00acbb0235ba49972d434361
96paradise <cnw0806@naver.com>
2025-04-25 11:17:27 +0900

    20250425 CNW

--- src/main/java/com/study/koreasleeptechservlet/order_app/dao/impl/UserDaoImpl.java
+++ src/main/java/com/study/koreasleeptechservlet/order_app/dao/impl/UserDaoImpl.java
@@ -8,38 +8,35 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.study.koreasleeptechservlet.dao.UserSql.INSERT;
-
 public class UserDaoImpl implements UserDao {
     private final Connection conn = DBConnection.getInstance().getConnection();
 
-    private final String INSERT_SQL = "INSERT INTO user (name, email) VALUES (?, ?)";
+    private final String INSERT = "INSERT INTO user (name, email) VALUES (?, ?)";
     private final String FIND_BY_EMAIL = "SELECT * FROM user WHERE email = ?";
     private final String FIND_BY_ID = "SELECT * FROM user WHERE id = ?";
     private final String FIND_ALL = "SELECT * FROM user";
 
     @Override
     public boolean save(User user) {
-        try(PreparedStatement pstmt = conn.prepareStatement(INSERT)) {
+        try (PreparedStatement pstmt = conn.prepareStatement(INSERT)) {
             pstmt.setString(1, user.getName());
             pstmt.setString(2, user.getEmail());
 
-            return pstmt.executeUpdate() > 0;
+            return pstmt.executeUpdate() > 0; // 저장된 경우 true, 그렇지 않은 경우 false 반환
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
         }
-
     }
 
     @Override
     public List<User> findAll() {
         List<User> users = new ArrayList<>();
-        try(Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(FIND_ALL)
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(FIND_ALL)
         ) {
             while(rs.next()) {
-                User user = new User (rs.getString("name"), rs.getString("email"));
+                User user = new User(rs.getString("name"), rs.getString("email"));
                 user.setId(rs.getInt("id"));
                 users.add(user);
             }
@@ -51,12 +48,12 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public User findByEmail(String email) {
-        try(PreparedStatement pstmt = conn.prepareStatement(FIND_BY_EMAIL)) {
+        try (PreparedStatement pstmt = conn.prepareStatement(FIND_BY_EMAIL)) {
             pstmt.setString(1, email);
             ResultSet rs = pstmt.executeQuery();
 
-            if(rs.next()) {
-                User user = new User (rs.getString("name"), rs.getString("email"));
+            if (rs.next()) {
+                User user = new User(rs.getString("name"), rs.getString("email"));
                 user.setId(rs.getInt("id"));
                 return user;
             }
@@ -73,10 +70,14 @@ public class UserDaoImpl implements UserDao {
             pstmt.setInt(1, id);
             ResultSet rs = pstmt.executeQuery();
 
-            if(rs.next()) {
-                User user = new User (rs.getString("name"), rs.getString("email"));
+            if (rs.next()) {
+                User user = new User(rs.getString("name"), rs.getString("email"));
                 user.setId(rs.getInt("id"));
                 return user;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return null;
     }
